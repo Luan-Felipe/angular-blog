feca15449c6c8b75cb36163deeb27edb7d2bc6a5
Luan-Felipe <luanfel2009@gmail.com>
2023-05-22 20:13:47 -0300

    adicionar pagina content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"sss",
+        "title":"sssssssssss",
+        "description":"ssssssss"
+    },
+    {
+        "id":2,
+        "photo":"aaaaaasss",
+        "title":"sssssssssssdfsssss",
+        "description":"sssssswewrss"
+    }
+]
